65dacd7123cf8b49332aa009660922f815140080
Valli Mac <vzradha@gmail.com>
2015-09-24 14:46:29 +1000

    chap-10 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
+  
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get  'about' => 'static_pages#about'
@@ -11,6 +10,7 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users  
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   
   # The priority is based upon order of creation: first created -> highest priority.
